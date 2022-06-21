6ed87f49bf7051413689c08e645025a10411b31e
Shu393 <107951701+Shu393@users.noreply.github.com>
2022-06-22 01:07:25 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# Boolean
+var_bool = True # Булевое переменная
+var_bool = False
+
+# integer
+var_int1 = 12
+var_int2 = -10
+
+# Float
+var_float1 = 12.12
+var_float2 = -10.52
+
+# list
+var_list1 = [12,10,1,-32]
+var_list2 = [100,True,1,-32]
+
+# String
+var_str1 = ""
+var_str2 = "fdfdf"
+var_str3 = "Test'WOW'Test"
+var_str4 = "12"
+
+# dict
+var_dict1 = {
+    "First key": "Иван",
+
+}
+
+var_dict2 = {
+    "First key": "Иван",
+    "Second key": "Иван",
+    "Third key": [100,True,1,-32],
+    "Forth key": {
+    "First key": "Иван",
+    "Second key": "Иван",
+    "Third key": [100,True,1,-32],
+    "Forth key": "Иван",
+
+},
+
+}
+
+# CONSTANT
+VAR_TUPLE = 12 # условно неизменяемая
+
+print(type(var_float1))
+print(var_float1)
